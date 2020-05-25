7640b23d89cf8f9137c0e6d0d4a87016aa3d8299
haowei0315 <476251935@qq.com>
2020-05-25 14:32:13 +0800

    通过config-server调试bus 刷新git服务器上config配置文件

--- microservice-config-server-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
+++ microservice-config-server-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * git上手动更改配置文件，
+ *  curl -X POST http://localhost:8080/bus/refresh
+ *  注：下面二个只能单机使用，不能通过mq刷新
+ * curl -X POST http://localhost:8080/actuator/refresh 用来测试刷新配置更改，
+ * 引入Cloud Bus后，就会多一个/actuator/bus-refresh 端点
+ *
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
